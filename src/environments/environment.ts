ff09883868d289ed4b86685a3647678fdf683a4c
Alex Reinoso <ralexjessiel@gmail.com>
2021-02-10 07:49:15 -0500

    Inicio evaluacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBR0K4IH39WdXE2Vkjd9vAeNlHWn1nL398",
+    authDomain: "ventasreinosoalex.firebaseapp.com",
+    projectId: "ventasreinosoalex",
+    storageBucket: "ventasreinosoalex.appspot.com",
+    messagingSenderId: "855267079102",
+    appId: "1:855267079102:web:8adc4de79029fc6576ffef"
+  }
+
 };
 
 /*
